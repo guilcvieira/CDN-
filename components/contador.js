63f9c60c01eaf8744c6d2cba36851c62c2cb8c55
Guilherme Vieira <guilcvieira@hotmail.com>
2025-04-07 16:33:40 -0300

    teste com transpilação

--- components/contador.js
+++ components/contador.js
@@ -1,16 +1,9 @@
-"use client"
-
 import React from 'https://esm.sh/react?dev';
-
 export default function Contador() {
-    const [contador, setContador] = React.useState(0);
-    return (
-        <div className="p-1 border">
-            <p>
-                Contador: {contador}
-            </p>
-
-            <button onClick={() => setContador(contador + 1)}>Clique Aqui</button>
-        </div>
-    )
+  const [contador, setContador] = React.useState(0);
+  return /*#__PURE__*/React.createElement("div", {
+    className: "p-1 border"
+  }, /*#__PURE__*/React.createElement("p", null, "Contador: ", contador), /*#__PURE__*/React.createElement("button", {
+    onClick: () => setContador(contador + 1)
+  }, "Clique Aqui"));
 }
