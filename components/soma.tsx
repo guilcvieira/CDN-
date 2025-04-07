bf2a179e7e6eef335b1eb9bcebfa9478ebefc96b
Guilherme Vieira <guilcvieira@hotmail.com>
2025-04-07 17:15:48 -0300

    teste sem build

---
+++ components/soma.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+export default function Soma(a: number, b: number) {
+  return (
+    <div className="soma">
+      <h1>Soma</h1>
+      <p>
+        {a} + {b} = {a + b}
+      </p>
+    </div>
+  );
+}
+
+window["startComponent"] = ({
+  container,
+  a,
+  b,
+}: {
+  container: any;
+  a: number;
+  b: number;
+}) => {
+  const element = document.getElementById(container);
+
+  if (!element) {
+    console.error(`Container with id ${container} not found.`);
+    return;
+  }
+
+  const root = createRoot(element);
+
+  root.render(
+    <React.StrictMode>
+      <Soma a={a} b={b} />
+    </React.StrictMode>
+  );
+};
