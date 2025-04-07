8e387183062d98e0a34aec60a02095fc8d440ad7
Guilherme Vieira <guilcvieira@hotmail.com>
2025-04-07 16:45:52 -0300

    teste tctoe

---
+++ components/tctoe.tsx
@@ -0,0 +1,125 @@
+// Instalar esses pacotes é opcional, mas é recomendado por conta do intelisense
+// Use npm install @types/react-dom
+import React, { StrictMode, useState } from "react";
+import { createRoot } from "react-dom/client";
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function Board({ xIsNext, squares, onPlay, playerName }) {
+  function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    nextSquares[i] = xIsNext ? "X" : "O";
+    onPlay(nextSquares);
+  }
+
+  const winner = calculateWinner(squares);
+  const status = winner
+    ? `Winner: ${winner} 🎉`
+    : `Next player: ${xIsNext ? playerName : "Opponent"}`;
+
+  return (
+    <>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        {[0, 1, 2].map((i) => (
+          <Square value={squares[i]} onSquareClick={() => handleClick(i)} />
+        ))}
+      </div>
+      <div className="board-row">
+        {[3, 4, 5].map((i) => (
+          <Square value={squares[i]} onSquareClick={() => handleClick(i)} />
+        ))}
+      </div>
+      <div className="board-row">
+        {[6, 7, 8].map((i) => (
+          <Square value={squares[i]} onSquareClick={() => handleClick(i)} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+function App({ playerName }) {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => (
+    <li key={move}>
+      <button onClick={() => jumpTo(move)}>
+        {move ? `Go to move #${move}` : "Go to game start"}
+      </button>
+    </li>
+  ));
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board
+          xIsNext={xIsNext}
+          squares={currentSquares}
+          onPlay={handlePlay}
+          playerName={playerName}
+        />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (const [a, b, c] of lines) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+ 
+window["startTicTacToe"] = ({ container, playerName }) => {
+  const elem = document.getElementById(container);
+
+  if (!elem) {
+    console.error(`Container with id ${container} not found.`);
+    return
+  }
+
+  const root = createRoot(elem);
+  root.render(
+    <StrictMode>
+      <App playerName={playerName} />
+    </StrictMode>
+  );
+};
